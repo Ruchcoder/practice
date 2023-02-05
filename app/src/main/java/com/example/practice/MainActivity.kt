3d26642f92798dd80849c947825e5cc7528dd268
Ruchinna <ruthokeniyi@gmail.com>
2023-02-05 16:26:23 +0100

    Initial commit pdf reader

---
+++ app/src/main/java/com/example/practice/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.practice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.VideoView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var VideoView:VideoView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        VideoView = findViewById(R.id.)
+    }
+}
